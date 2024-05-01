ed13a6ef3a82b8188c1997e20fc19481d93e875a
yyyn012 <111561826+yyyn012@users.noreply.github.com>
2024-05-01 16:15:56 +0900

    menuScroll.js

--- kakaoBank/js/menuScroll.js
+++ kakaoBank/js/menuScroll.js
@@ -2,10 +2,19 @@ const header = document.querySelector(".header");
 const headerHeight = header.getBoundingClientRect().height;
 
 // body scroll 시 header background color 바뀌게 하기
-document.addEventListener("scroll", () => {
+const headerScroll = () => {
   if (window.scrollY > headerHeight) {
     header.classList.add("active");
   } else {
     header.classList.remove("active");
   }
-});
+};
+
+const headerColor = () => {
+  header.style.backgroundColor = "#fff";
+  header.style.borderBottom = "1px solid e6e3e3";
+};
+
+document.addEventListener("scroll", headerScroll);
+
+window.addEventListener("load", headerColor);
