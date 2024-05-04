14187f207c559ce48557ccaea68d1524f1681880
yyyn012 <111561826+yyyn012@users.noreply.github.com>
2024-05-04 16:50:18 +0900

    nav open

--- kakaoBank/js/openMenu.js
+++ kakaoBank/js/openMenu.js
@@ -3,18 +3,17 @@ const navBtn = document.querySelector(".nav-btn");
 const nav = document.querySelector(".nav");
 const headerColor = document.querySelector(".header");
 
-// menu open 시 navBtn event, menu hidden, body scroll 제거, header backgroundColor white로 변경
+// menu open 시 navBtn event, menu hidden, body scroll 제거, header backgroundColor 변경
 function openMenu() {
   if (nav.classList.contains("active")) {
     body.classList.remove("noScroll");
     nav.classList.remove("active");
     navBtn.classList.remove("active");
-    headerColor.classList.remove("header-color");
   } else {
     body.classList.add("noScroll");
     nav.classList.add("active");
     navBtn.classList.add("active");
-    headerColor.classList.add("header-color");
+    headerColor.style.backgroundColor = "rgba(255, 255, 255, 1)";
   }
 }
 
