9b92c617677bb47c5d8bdd56243deee265deaf85
yyyn012 <111561826+yyyn012@users.noreply.github.com>
2024-05-05 15:46:33 +0900

    skySlide.js and ico_star

--- kakaoBank/js/skySlide.js
+++ kakaoBank/js/skySlide.js
@@ -4,8 +4,7 @@ const intersectionObserver = new IntersectionObserver(function (entries) {
   // intersectionRatio가 0이라는 것은 대상을 볼 수 없다는 것이므로 아무것도 하지 않음
   if (entries[0].intersectionRatio <= 0) return;
 
-  blue.classList.add("blue active");
-  // blue.classList.remove("blue");
+  blue.style.height = "0";
 });
 
 // 주시 시작
